f740091d0e35ca0a06b2f5cde46d2881e4330701
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-18 22:57:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carga_de_credito',
+  webDir: 'www'
+};
+
+export default config;
